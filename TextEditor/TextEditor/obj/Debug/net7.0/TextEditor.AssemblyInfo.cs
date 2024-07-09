6b24a114b4cc2bd99b6e298cdd9bca6c0d7e778d
Hamed Ahmed <ha9597ma@gmail.com>
2024-07-09 11:38:01 +0300

    Fixing Index page and Modify Delete Method

--- TextEditor/TextEditor/obj/Debug/net7.0/TextEditor.AssemblyInfo.cs
+++ TextEditor/TextEditor/obj/Debug/net7.0/TextEditor.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fa9799d0843be1b2fc454c0134d9d40799e560d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567805de4635724429c3c2b08910da337fe04b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
